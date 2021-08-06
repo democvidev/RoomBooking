51994b73f7c937371bd3ee7948869c012a1edeb0
cvidev <cnvvc_vlad@mail.ru>
2021-08-06 20:59:28 +0200

    add fields & updated view

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,6 +17,8 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+        ->add('lastName')
+        ->add('firstName')
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
